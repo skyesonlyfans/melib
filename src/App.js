4339ee237272c295aafbcf9a1eeb50a6f9bc32ed
Skye N <tylern662@gmail.com>
2025-09-01 03:18:06 -0400

    first push

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchPage from './SearchPage';
-import ReaderPage from './ReaderPage';
+import SearchPage from './pages/SearchPage';
+import ReaderPage from './pages/ReaderPage';
 
 function App() {
   return (
@@ -19,4 +19,3 @@ function App() {
 }
 
 export default App;
-
